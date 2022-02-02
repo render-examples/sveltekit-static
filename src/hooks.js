2dea97ddb1b4b11b43816a936940b1ac34656c55
metonym <ericyl.us@gmail.com>
2022-02-02 08:46:56 -0800

    headers.cookie -> haders.get('cookie')

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@ import cookie from 'cookie';
 import { v4 as uuid } from '@lukeed/uuid';
 
 export const handle = async ({ event, resolve }) => {
-	const cookies = cookie.parse(event.request.headers.cookie || '');
+	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 	event.locals.userid = cookies.userid || uuid();
 
 	const response = await resolve(event);
