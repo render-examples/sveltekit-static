2109623668c961cbb3d23c0fd7ef769a86be1ddc
metonym <ericyl.us@gmail.com>
2022-08-09 16:14:05 -0700

    Upgrade SvelteKit/Vite to the latest versions (#15)
    * Upgrade dependencies to latest
    
    * Remove port flag from dev command
    
    * Update location of JSDoc type in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
-/** @type {import('@sveltejs/kit').Config} */
-
 import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
